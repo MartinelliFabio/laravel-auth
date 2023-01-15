27b35b17afa0f7b0edf5ac20cdd1cbbcc248e20c
MartinelliFabio <fabio.martinelli996@gmail.com>
2023-01-15 18:19:08 +0100

    Make migrations and SeederType

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -4,19 +4,19 @@
     <div class="row g-4">
         <div class="col-4 d-flex justify-content-center align-items-center">
             <div class="container card-box">
-                <img class="my-img" src="{{ asset('storage/' . $project->cover_image) }}" alt="">
+                <img src="{{ asset('storage/' . $project->cover_image) }}" alt="">
                 <div>{{ $project->name }}</div>
                 <div>{{ $project->diff_lvl }}</div>
-                <div><a href="{{ route('admin.projects.show', $project->slug) }}">{{ $project->slug }}</a></div>
+                <div>{{ $project->slug }}</div>
                 <div>{{ $project->dev_lang }}</div>
                 <div>{{ $project->framework }}</div>
                 <div>{{ $project->team }}</div>
                 <div>{{ $project->git_link }}</div>
                 <div>{{ $project->description }}</div>
             </div>
-            <button class="btn btn-primary mt-3">
-                <a href="{{ route('admin.projects.index') }}"style="color:white">Indietro</a>
-            </button>
+            <div>
+                <button class="btn btn-primary mt-3"><a href="{{ route('admin.projects.index') }}"style="color:white">Indietro</a></button>
+            </div>
         </div>
     </div>
 </div>
